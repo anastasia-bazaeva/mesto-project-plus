bcf8875c3a834054f9667155f0fcac4eb3b283b9
Anastasiya Bazaeva <anastasiya.albertowna@yandex.ru>
2023-04-26 17:00:57 +0300

    decorators for user update added

--- src/controllers/user.ts
+++ src/controllers/user.ts
@@ -41,10 +41,19 @@ export const createUser = (req: Request, res: Response) => {
   })
 }
 
-export const updateUserInfo = (req: RequestWithUserRole, res: Response) => {
+export function infoUpdate (req: RequestWithUserRole, res: Response) {
   const { name, about } = req.body;
+  return updateUser(req, res, { name, about })
+}
+
+export function avatarUpdate (req: RequestWithUserRole, res: Response) {
+  const { avatar } = req.body;
+  return updateUser(req, res, { avatar })
+}
+
+const updateUser = (req: RequestWithUserRole, res: Response, option: Object) => {
   return userSchema.findByIdAndUpdate({ _id: req.user?._id },
-    { name, about },
+    option,
     { new: true, runValidators: true }, )
     .then(user => res.status(SUCCESS).send({ data: user }))
     .catch((err) => {
@@ -55,22 +64,5 @@ export const updateUserInfo = (req: RequestWithUserRole, res: Response) => {
         return res.status(BAD_REQUEST).send(validationErrorHandler(err))
       }
       res.status(SERVER_ERROR).send({ message: 'Произошла ошибка при обновлении данных профиля' })
-    })
-}
-
-export const updateAvatar = (req: RequestWithUserRole, res: Response) => {
-  const { avatar } = req.body;
-  return userSchema.findByIdAndUpdate({ _id: req.user?._id },
-    { avatar },
-    { new: true, runValidators: true })
-    .then(user => res.status(SUCCESS).send({ data: user }))
-    .catch((err) => {
-      if(err instanceof mongoose.Error.DocumentNotFoundError){
-        return res.status(NOT_FOUND).send({ message: 'Пользователь не найден' })
-      }
-      if(err instanceof mongoose.Error.ValidationError) {
-        return res.status(BAD_REQUEST).send(validationErrorHandler(err))
-      }
-      res.status(SERVER_ERROR).send({ message: 'Произошла ошибка при обновлении аватара' })
     })
 }
